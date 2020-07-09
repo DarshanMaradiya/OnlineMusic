f67b445f614802373fef4499773bcf7b78a35aa1
Darshan Maradiya <66081547+DarshanMaradiya@users.noreply.github.com>
2020-07-10 01:43:28 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -24,8 +24,8 @@ function verifyMail($id, $mailAddress, $name = "")
 	    $mail->isSMTP();                                            // Send using SMTP
 	    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
 	    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-	    $mail->Username   = 'foreverlearners1234@gmail.com';                     // SMTP username
-	    $mail->Password   = 'qwerty@1234';                               // SMTP password
+	    $mail->Username   = 'something@gmail.com';                     // SMTP username
+	    $mail->Password   = 'password';                               // SMTP password
 	    $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 	    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
