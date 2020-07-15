89bf6ee2fd9794320b97f3e56244d5553f381a44
Darshan <darshan250999@gmail.com>
2020-07-15 16:32:54 +0530

    New Feature come Modification : Search in specific - search in artists - search in songs i

--- search.php
+++ search.php
@@ -28,11 +28,20 @@
 <?php
 
 	include('dbcon.php');
-
-
 	$stext = $_POST['stext'];
 
-	$sql = "SELECT * FROM `songinfo` WHERE `sname` LIKE '%$stext%'";
+?>
+<body>
+<h1>Search Results for "<?php echo $stext;?>"</h1>
+<button class="allBtn" onclick="searchAll();">Search "<?php echo $stext;?>" in All</button>
+<button class="artistBtn" onclick="searchArtists();">Search "<?php echo $stext;?>" in artist</button>
+<button class="songBtn" onclick="searchSongs();">Search "<?php echo $stext;?>" in songs</button>
+<br>
+<br>
+<div class="searchResults">
+<?php
+
+	$sql = "SELECT * FROM `songinfo` WHERE `sname` LIKE '%$stext%' or `artist` LIKE '%$stext%'";
 	$run = mysqli_query($con,$sql);
 	$row = mysqli_num_rows($run);
 	if($row)
@@ -41,39 +50,69 @@
 			{
 
 				?>
-				<a href="music/songinfo.php?scode=<?php echo $data['scode'];?>"><?php echo $data['sname'];?></a><?php echo " by ".$data['artist'];?>
-					<br>
-					<br>
+				<div class="result" style = "margin-bottom: 15px; ">
+				<a href="music/songinfo.php?scode=<?php echo $data['scode'];?>"><?php echo $data['sname'];?></a><?php echo "  by ".$data['artist'];?>
+				</div>
 				<?php
 			}
 	}
-	else
+
+	if($row == 0)
 	{
-		$sql = "SELECT * FROM `songinfo` WHERE `artist` LIKE '%$stext%'";
-		$run = mysqli_query($con,$sql);
-		$row = mysqli_num_rows($run);
-		if($row)
-		{
-			while($data=mysqli_fetch_assoc($run))
+		?>
+		<a href="index.php">Back to Home</a>
+		<h3><center>No Result Found!</center></h3>
+		<?php
+	}
+?>
+</div>
+<script type="text/javascript">
+	var results = document.getElementsByClassName("result");
+	function searchArtists()
+	{
+		var key = "<?php echo $stext;?>";
+		for(var result of results){
+			if(result.textContent.toLowerCase().split("by")[1].includes(key))
 			{
-
-				?>
-				<a href="music/songinfo.php?scode=<?php echo $data['scode'];?>"><?php echo $data['sname'];?></a><?php echo " by ".$data['artist'];?>
-					<br>
-					<br>
-				<?php
+				result.style.display = "block";
+			}
+			else
+			{
+				result.style.display = "none";
 			}
 		}
-		else
-		{
-			?>
-			<a href="index.php">Back to Home</a>
-			<h3><center>No Result Found!</center></h3>
-			<?php
-		}
+		document.getElementsByTagName("h1")[0].textContent = "Search Results for \"<?php echo $stext;?>\" in artists";
+		document.getElementsByClassName("artistBtn")[0].style.display = "none";
+		document.getElementsByClassName("songBtn")[0].style.display = "inline-block";
 
 	}
-
-?>
-
+	function searchSongs()
+	{
+		var key = "<?php echo $stext;?>";
+		for(var result of results){
+			if(result.textContent.toLowerCase().split("by")[0].includes(key))
+			{
+				result.style.display = "block";
+			}
+			else
+			{
+				result.style.display = "none";
+			}
+		}
+		document.getElementsByTagName("h1")[0].textContent = "Search Results for \"<?php echo $stext;?>\" in songs";
+		document.getElementsByClassName("songBtn")[0].style.display = "none";
+		document.getElementsByClassName("artistBtn")[0].style.display = "inline-block";
+	}
+	function searchAll()
+	{
+		var key = "<?php echo $stext;?>";
+		for(var result of results){
+			result.style.display = "block";
+		}
+		document.getElementsByTagName("h1")[0].textContent = "Search Results for \"<?php echo $stext;?>\"";
+		document.getElementsByClassName("songBtn")[0].style.display = "inline-block";
+		document.getElementsByClassName("artistBtn")[0].style.display = "inline-block";
+	}
+</script>
+</body>
 </html>
